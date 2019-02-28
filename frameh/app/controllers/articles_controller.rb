a14029c79beec62a16956927fd40bc4e1b6099cd
Hugo Morais <hugodanielmorais@gmail.com>
2019-02-28 22:31:29 +0000

    Article actions - create, show and form

--- frameh/app/controllers/articles_controller.rb
+++ frameh/app/controllers/articles_controller.rb
@@ -1,22 +1,27 @@
 class ArticlesController < ApplicationController
-  
-  def index
-    
-  end
-  
-  
+
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
-  def read
+  def show
+    @article = Article.find(params[:id])
   end
 
-  def update
+  private
+
+  def article_params
+    params.require(:article).permit!
   end
 
-  def destroy
-  end
 end
